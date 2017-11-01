ced9f4b6179599e7bbe7cd1279411b3ff5021893
kwlee <kwlee@etri.re.kr>
2017-11-01 17:06:59 +0900

    .

--- bin/windows/run_sample.bat
+++ bin/windows/run_sample.bat
@@ -1,3 +1,3 @@
 echo "Running %1..."
 
-java -cp %MARMOT_CLIENT_HOME%/bin/marmot.client.jar;%MARMOT_SAMPLE_HOME%/build/libs/marmot.sample-1.2.jar %1 %2 %3 %4 %5
+java -cp "%MARMOT_CLIENT_HOME%/bin/marmot.client.jar;%MARMOT_SAMPLE_HOME%/build/libs/marmot.sample-1.2.jar" %1 %2 %3 %4 %5
