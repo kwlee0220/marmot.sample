08c44e8deea9d658cbed95477adb3856b6c537c5
kwlee <kwlee@node99.gsbd.anyang.ac.kr>
2018-09-18 10:14:20 +0900

    .

--- src/main/java/demo/WeakFireDeathArea.java
+++ src/main/java/demo/WeakFireDeathArea.java
@@ -48,18 +48,18 @@ public class WeakFireDeathArea {
 		Plan plan0 = marmot.planBuilder("서울시 종합병원 위치에서 3km 버퍼 연산")
 								.load(LAYER_HOSPITAL)
 								.buffer("the_geom", 3000)
-								.store("tmp/weak_area/hospital3000")
+								.store("tmp/종합병원_위치_추천/병원_3km버퍼")
 								.build();
-		marmot.createDataSet("tmp/weak_area/hospital3000", gcInfo, plan0, true);
+		marmot.createDataSet("tmp/종합병원_위치_추천/병원_3km버퍼",  gcInfo, plan0, true);
 		System.out.println("완료: 서울시 종합병원 위치에서 3km 버퍼 영역 생성");
 
 		// 서울시 지도에서 종합병원 3km 이내 영역과 겹치지 않은 영역을 계산한다.
 		Plan plan1 = marmot.planBuilder("종합병원 3km 밖의 서울시 영역 계산")
 								.load(LAYER_SEOUL)
-								.differenceJoin("the_geom", "tmp/weak_area/hospital3000")
-								.store("tmp/weak_area/far_seoul")
+								.differenceJoin("the_geom", "tmp/종합병원_위치_추천/병원_3km버퍼")
+								.store("tmp/종합병원_위치_추천/병원_원거리_영역")
 								.build();
-		marmot.createDataSet("tmp/weak_area/far_seoul", gcInfo, plan1, true);
+		marmot.createDataSet("tmp/종합병원_위치_추천/병원_원거리_영역", gcInfo, plan1, true);
 		System.out.println("완료: 종합병원 3km 밖의 서울시 영역 계산");
 
 		// 화재피해 영역 중에서 서울 읍면동과 겹치는 부분을 clip 하고, 각 피해 영역의 중심점을 구한다.
@@ -68,14 +68,14 @@ public class WeakFireDeathArea {
 								.filter("sd_nm == '서울특별시'")
 								.clipJoin("the_geom", LAYER_SEOUL)
 								.centroid("the_geom")
-								.clipJoin("the_geom", "tmp/weak_area/far_seoul")
-								.store("tmp/weak_area/result")
+								.clipJoin("the_geom", "tmp/종합병원_위치_추천/병원_원거리_영역")
+								.store("tmp/종합병원_위치_추천/분석결과")
 								.build();
-		DataSet result = marmot.createDataSet("tmp/weak_area/result", gcInfo, plan2, true);
+		DataSet result = marmot.createDataSet("tmp/종합병원_위치_추천/분석결과", gcInfo, plan2, true);
 		System.out.println("완료: 종합병원 3km 밖의 서울시 영역과 화재피해 영역과 교차부분 검색");
 		
-		marmot.deleteDataSet("tmp/weak_area/hospital3000");
-		marmot.deleteDataSet("tmp/weak_area/far_seoul");
+		marmot.deleteDataSet("tmp/종합병원_위치_추천/병원_3km버퍼");
+		marmot.deleteDataSet("tmp/종합병원_위치_추천/병원_원거리_영역");
 		
 		watch.stop();
 		System.out.printf("elapsed time=%s%n", watch.getElapsedMillisString());
