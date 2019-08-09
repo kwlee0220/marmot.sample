4be2a1b9bf89b5820a1b004169e5f9c5247904f1
kwlee <kwlee@etri.re.kr>
2019-08-09 16:40:14 +0900

    text 기반 MR output compress 기능 추가

---
+++ src/main/java/marmot/SampleStoreAsCsv.java
@@ -0,0 +1,41 @@
+package marmot;
+
+import static marmot.optor.StoreAsCsvOptions.DEFAULT;
+
+import org.apache.log4j.PropertyConfigurator;
+
+import marmot.command.MarmotClientCommands;
+import marmot.remote.protobuf.PBMarmotClient;
+
+/**
+ * 
+ * @author Kang-Woo Lee (ETRI)
+ */
+public class SampleStoreAsCsv {
+	private static final String INPUT = "교통/지하철/서울역사";
+	private static final String RESULT = "tmp/result";
+	
+	public static final void main(String... args) throws Exception {
+		PropertyConfigurator.configure("log4j.properties");
+
+		// 원격 MarmotServer에 접속.
+		PBMarmotClient marmot = MarmotClientCommands.connect();
+
+		Plan plan;
+		plan = marmot.planBuilder("test")
+					.load(INPUT)
+					.filter("sub_sta_sn > 320")
+					.project("kor_sub_nm,sub_sta_sn,sig_cd")
+					.storeAsCsv(RESULT, DEFAULT('@'))
+					.build();
+		marmot.execute(plan);
+		
+		plan = marmot.planBuilder("test")
+					.load(INPUT)
+					.filter("sub_sta_sn > 320")
+					.project("kor_sub_nm,sub_sta_sn,sig_cd")
+					.storeAsCsv(RESULT, DEFAULT(',').compressionCodecName("gzip"))
+					.build();
+		marmot.execute(plan);
+	}
+}
