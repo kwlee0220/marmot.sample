1f5d40242054f7fe533ff00a7753bcdd7b8e439f
kwlee <kwlee@etri.re.kr>
2019-12-17 15:29:57 +0900

    .

--- src/main/java/marmot/SampleGetAnalysisParameters.java
+++ src/main/java/marmot/SampleGetAnalysisParameters.java
@@ -21,7 +21,7 @@ public class SampleGetAnalysisParameters {
 
 //		marmot.deleteAnalysisAll();
 		
-		for ( String id: marmot.getSystemAnalysisClassIdAll() ) {
+		for ( String id: marmot.getSystemAnalsisClassIdAll() ) {
 			System.out.println("system analysis: " + id);
 			System.out.println("\targs: " + marmot.getSystemAnalysisParameterNameAll(id));
 		}
