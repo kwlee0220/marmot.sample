fa3b6e922a5e367001eee9f13778eb195c2eaf4f
kwlee <kwlee@etri.re.kr>
2019-12-17 15:11:11 +0900

    .

---
+++ src/main/java/marmot/advanced/SampleLoadKafkaDataSet.java
@@ -0,0 +1,42 @@
+package marmot.advanced;
+
+import static marmot.optor.StoreDataSetOptions.FORCE;
+
+import org.apache.log4j.PropertyConfigurator;
+
+import common.SampleUtils;
+import marmot.Plan;
+import marmot.command.MarmotClientCommands;
+import marmot.dataset.DataSet;
+import marmot.dataset.GeometryColumnInfo;
+import marmot.remote.protobuf.PBMarmotClient;
+
+/**
+ * 
+ * @author Kang-Woo Lee (ETRI)
+ */
+public class SampleLoadKafkaDataSet {
+	private static final String INPUT = "주소/건물POI";
+	private static final String RESULT = "tmp/result";
+	
+	public static final void main(String... args) throws Exception {
+		PropertyConfigurator.configure("log4j.properties");
+
+		// 원격 MarmotServer에 접속.
+		PBMarmotClient marmot = MarmotClientCommands.connect();
+		
+		DataSet input = marmot.getDataSet(INPUT);
+		GeometryColumnInfo gcInfo = input.getGeometryColumnInfo();
+		
+		Plan plan = Plan.builder("sample_assign_uid")
+							.load(INPUT)
+							.filter("(long)출입구일련번호 % 119999 == 3")
+							.assignUid("guid")
+							.project("the_geom,guid,출입구일련번호")
+							.store(RESULT, FORCE(gcInfo))
+							.build();
+		marmot.execute(plan);
+		DataSet result = marmot.getDataSet(RESULT);
+		SampleUtils.printPrefix(result, 5);
+	}
+}
