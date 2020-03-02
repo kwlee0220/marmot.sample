3f8d066aa636a9da72184e093d09fe8dbbc83cd6
kwlee <kwlee@etri.re.kr>
2020-03-02 12:20:26 +0900

    .

--- src/main/java/marmot/geom/advanced/SampleEstimateQuadKeys.java
+++ src/main/java/marmot/geom/advanced/SampleEstimateQuadKeys.java
@@ -2,23 +2,29 @@ package marmot.geom.advanced;
 
 import org.apache.log4j.PropertyConfigurator;
 
+import com.vividsolutions.jts.geom.Envelope;
+
 import common.SampleUtils;
 import marmot.Plan;
 import marmot.command.MarmotClientCommands;
 import marmot.dataset.DataSet;
 import marmot.dataset.GeometryColumnInfo;
+import marmot.geo.CoordinateTransform;
 import marmot.optor.StoreDataSetOptions;
 import marmot.remote.protobuf.PBMarmotClient;
 import utils.StopWatch;
 import utils.UnitUtils;
+import utils.func.FOption;
 
 /**
  * 
  * @author Kang-Woo Lee (ETRI)
  */
 public class SampleEstimateQuadKeys {
+	private static final String SIDO = "구역/시도";
 //	private static final String INPUT = "교통/dtg_201609";
-	private static final String INPUT = "교통/지하철/서울역사";
+	private static final String INPUT = "나비콜/택시로그";
+//	private static final String INPUT = "교통/지하철/서울역사";
 //	private static final String INPUT = "주소/건물POI";
 	private static final String OUTPUT = "tmp/result";
 	
@@ -32,12 +38,20 @@ public class SampleEstimateQuadKeys {
 		
 		DataSet input = marmot.getDataSet(INPUT);
 		GeometryColumnInfo gcInfo = input.getGeometryColumnInfo();
+		
+		DataSet refDs = marmot.getDataSet(SIDO);
+		Envelope bounds = refDs.getBounds();
+		if ( !refDs.getGeometryColumnInfo().srid().equals(gcInfo.srid()) ) {
+			bounds = CoordinateTransform.get(refDs.getGeometryColumnInfo().srid(), gcInfo.srid())
+										.transform(bounds);
+		}
+		
 		long clusterSize = UnitUtils.parseByteSize("64mb");
 		
 		Plan plan;
 		plan = Plan.builder("test_estimate_quadkeys")
 					.load(INPUT)
-					.estimateQueryKeys(gcInfo, 0.001, 17, clusterSize)
+					.estimateQueryKeys(gcInfo, 0.001, FOption.of(bounds), 17, clusterSize)
 					.store(OUTPUT, StoreDataSetOptions.FORCE(gcInfo))
 					.build();
 		marmot.execute(plan);
--- src/main/java/marmot/module/SampleClusterSpatialDataSet.java
+++ src/main/java/marmot/module/SampleClusterSpatialDataSet.java
@@ -2,9 +2,13 @@ package marmot.module;
 
 import org.apache.log4j.PropertyConfigurator;
 
+import com.vividsolutions.jts.geom.Envelope;
+
 import marmot.analysis.module.geo.ClusterSpatialDataSetParameters;
 import marmot.command.MarmotClientCommands;
 import marmot.dataset.DataSet;
+import marmot.dataset.GeometryColumnInfo;
+import marmot.geo.CoordinateTransform;
 import marmot.remote.protobuf.PBMarmotClient;
 import utils.StopWatch;
 import utils.UnitUtils;
@@ -14,9 +18,15 @@ import utils.UnitUtils;
  * @author Kang-Woo Lee (ETRI)
  */
 public class SampleClusterSpatialDataSet {
-	private static final String INPUT = "교통/dtg_201609";
+	private static final String BOUNDS = "구역/시도";
+//	private static final String QUAD_INPUT = "지오비전/집계구/2015_clustered";
+//	private static final String BOUNDS = "교통/지하철/서울역사";
+//	private static final String INPUT = "교통/dtg_201609";
+	private static final String INPUT = "나비콜/택시로그";
 //	private static final String INPUT = "주소/건물POI";
 //	private static final String INPUT = "POI/병원";
+//	private static final String INPUT = "지오비전/집계구/2015";
+//	private static final String INPUT = "구역/시군구";
 //	private static final String OUTPUT = "tmp/result";
 	
 	public static final void main(String... args) throws Exception {
@@ -28,22 +38,35 @@ public class SampleClusterSpatialDataSet {
 		StopWatch watch = StopWatch.start();
 		
 		DataSet input = marmot.getDataSet(INPUT);
-		long clusterSize = UnitUtils.parseByteSize("4gb");
-		long sampleSize = UnitUtils.parseByteSize("256mb");
-		long blockSize = UnitUtils.parseByteSize("128mb");
+		GeometryColumnInfo gcInfo = input.getGeometryColumnInfo();
+		long clusterSize = UnitUtils.parseByteSize("64mb");
+		long sampleSize = UnitUtils.parseByteSize("64mb");
+		long blockSize = UnitUtils.parseByteSize("64mb");
+//		long clusterSize = UnitUtils.parseByteSize("4gb");
+//		long sampleSize = UnitUtils.parseByteSize("256mb");
+//		long blockSize = UnitUtils.parseByteSize("128mb");
 		int workerCount = 53;
+		
+		DataSet refDs = marmot.getDataSet(BOUNDS);
+		Envelope bounds = refDs.getBounds();
+		if ( !refDs.getGeometryColumnInfo().srid().equals(gcInfo.srid()) ) {
+			bounds = CoordinateTransform.get(refDs.getGeometryColumnInfo().srid(), gcInfo.srid())
+										.transform(bounds);
+		}
 		
 		ClusterSpatialDataSetParameters params = new ClusterSpatialDataSetParameters();
 		params.inputDataSet(INPUT);
 		params.outputDataSet(INPUT + "_clustered");
+		params.validBounds(bounds);
 		params.maxQuadKeyLength(17);
 		params.clusterSize(clusterSize);
 		params.sampleSize(sampleSize);
+//		params.quadKeyDataSet(QUAD_INPUT);
 		params.force(true);
 		params.compressionCodecName("lz4");
 		params.blockSize(blockSize);
 		params.workerCount(workerCount);
-		marmot.executeProcess("cluster_spatial_dataset", params.toMap());
+		marmot.executeProcess(ClusterSpatialDataSetParameters.moduleName(), params.toMap());
 		
 		System.out.printf("elapsed=%s%n", watch.getElapsedMillisString());
 	}
