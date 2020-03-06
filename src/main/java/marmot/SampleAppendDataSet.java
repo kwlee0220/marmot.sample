b7b2af43eb536bcc300c05d1f68d2f123ebc555e
kwlee <kwlee@etri.re.kr>
2020-03-06 14:53:24 +0900

    파티션 지정 append기능 추가

---
+++ src/main/java/marmot/SampleAppendDataSet.java
@@ -0,0 +1,44 @@
+package marmot;
+
+import org.apache.log4j.PropertyConfigurator;
+
+import marmot.command.MarmotClientCommands;
+import marmot.dataset.DataSet;
+import marmot.optor.CreateDataSetOptions;
+import marmot.remote.protobuf.PBMarmotClient;
+import utils.StopWatch;
+
+/**
+ * 
+ * @author Kang-Woo Lee (ETRI)
+ */
+public class SampleAppendDataSet {
+	private static final String INPUT = "교통/지하철/서울역사";
+	private static final String RESULT = "tmp/result";
+	
+	public static final void main(String... args) throws Exception {
+		PropertyConfigurator.configure("log4j.properties");
+
+		// 원격 MarmotServer에 접속.
+		PBMarmotClient marmot = MarmotClientCommands.connect();
+		
+		StopWatch watch = StopWatch.start();
+		DataSet input = marmot.getDataSet(INPUT);
+		
+		DataSet created = marmot.createDataSet(RESULT, input.getRecordSchema(),
+												CreateDataSetOptions.FORCE(input.getGeometryColumnInfo()));
+		
+		try ( RecordSet rset = input.read() ) {
+			System.out.println("created: " + created.append(rset));
+		}
+		try ( RecordSet rset = input.read() ) {
+			System.out.println("created: " + created.append(rset, "kafka"));
+		}
+		try ( RecordSet rset = input.read() ) {
+			System.out.println("created: " + created.append(rset, "kafka"));
+		}
+		
+		created = marmot.getDataSet(RESULT);
+		System.out.println("total count=" + created.getRecordCount() + ", elapsed=" + watch.stopAndGetElpasedTimeString());
+	}
+}
